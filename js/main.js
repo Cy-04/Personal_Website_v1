a134efa8a06ec1fe15afc436a8b71e18a03ad43a
cyrin <amboscyrin@gmail.com>
2025-11-30 15:36:35 +0800

    Add on scroll reveal effect for services section

--- js/main.js
+++ js/main.js
@@ -54,3 +54,22 @@ carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 //CAROUSEL EVENTS END
+
+//EFFECTS - REVEAL ELEMENTS ON SCROLL
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowHeight - revealpoint){
+            reveals[i].classList.add('active');
+        }else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
