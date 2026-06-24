dc962a5386cfacf2dd6154e261cc6fa008dc70b9
cyrin <amboscyrin@gmail.com>
2026-06-24 22:03:16 +0800

    Fix lightbox for ios safari 5th attempt

--- js/action-lightbox.js
+++ js/action-lightbox.js
@@ -75,11 +75,10 @@
     buildStrip();
 
     nodes.forEach((img, i) => {
-      const trigger = img.closest('a, .gallery-item') || img;
+      const trigger = img.closest('p, .gallery-item') || img;
       trigger.style.cursor = 'pointer';
       trigger.addEventListener('click', (e) => {
         e.preventDefault();
-        e.stopPropagation() 
         openAt(i);
       });
     });
