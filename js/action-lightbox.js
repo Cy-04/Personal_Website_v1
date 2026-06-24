4d55d5017d9f9d0749da345901a80e979b51898d
cyrin <amboscyrin@gmail.com>
2026-06-24 22:09:16 +0800

    last attemp in fixing lightbox not working on ios

--- js/action-lightbox.js
+++ js/action-lightbox.js
@@ -75,7 +75,7 @@
     buildStrip();
 
     nodes.forEach((img, i) => {
-      const trigger = img.closest('a, .gallery-item') || img;
+      const trigger = img.closest('button, .gallery-item') || img;
       trigger.style.cursor = 'pointer';
       trigger.addEventListener('click', (e) => {
         e.preventDefault();
